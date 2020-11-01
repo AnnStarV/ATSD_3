2179b9e984ddd05b75c6abb374753d80b8dbb3e1
AnnStarV <anastarsova8@gmail.com>
2020-11-01 17:44:28 +0300

    mes

--- app/app.py
+++ app/app.py
@@ -1,22 +1,33 @@
+import datetime
+
 members = {
-    'first': ('01/11/2020', '05/11/2020', 1000),
-    'second': ('03/11/2020', '07/11/2020', 1200),
-    'third': ('08/11/2020', '11/11/2020', 1300),
-    'fourth': ('12/11/2020', '15/11/2020', 800),
-    'fifth': ('16/11/2020', '19/11/2020', 2000),
-    'sixth': ('20/11/2020', '22/11/2020', 1700),
-    'seventh': ('23/11/2020', '26/11/2020', 1600),
-    'eighth': ('25/11/2020', '28/11/2020', 1500),
-    'ninth': ('26/11/2020', '30/11/2020', 1250),
-    'tenth': ('27/11/2020', '31/11/2020', 1900)
+    'first': ('2020/11/01', '2020/11/03', 1000),
+    'second': ('2020/11/02', '2020/11/04', 1200),
+    'third': ('2020/11/05', '2020/11/07', 1300),
+    'fourth': ('2020/11/07', '2020/11/09', 800),
+    'fifth': ('2020/11/08', '2020/11/10', 2000),
+    'sixth': ('2020/11/11', '2020/11/13', 1700),
+    'seventh': ('2020/11/14', '2020/11/16', 1600),
+    'eighth': ('2020/11/17', '2020/11/20', 1500),
+    'ninth': ('2020/11/19', '2020/11/23', 1250),
+    'tenth': ('2020/11/24', '2020/11/27', 1900)
 }
 
 
-def get_days_and_money(part):
-    days = [part[item][1] - part[item][0] for item in part]
-    money = [part[item][2] for item in part]
-    print("days")
+def date_diff(first, second):
+    first = first.split('/')
+    second = second.split('/')
+    first = datetime.date(int(first[0]), int(first[1]), int(first[2]))
+    second = datetime.date(int(second[0]), int(second[1]), int(second[2]))
+    diff = first - second
+
+    return diff.days
+
+
+def get_days_and_money(members):
+    days = [date_diff(members[item][1], members[item][0]) for item in members]
+    money = [members[item][2] for item in members]
     return days, money
 
 
-get_days_and_money(members)
+print(get_days_and_money(members))
